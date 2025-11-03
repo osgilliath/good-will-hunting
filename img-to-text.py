61176601723329190d73cfa81ee2b7a8f98b8d75
osgilliath <shankhdhar.madhav@gmail.com>
2025-11-03 22:32:11 +0530

    removed img-to-text.py file

--- img-to-text.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-import pytesseract
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-print(pytesseract.image_to_string(Image.open('test.jpg')))
-
